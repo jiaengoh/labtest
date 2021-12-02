bd80c513678d0e51fa36be4d44169eec23364755
jiaengoh <57712490+jiaengoh@users.noreply.github.com>
2021-12-02 13:51:33 +0800

    s

--- src/index.php
+++ src/index.php
@@ -1,3 +1,15 @@
-<form method="POST">
-search: <input type="text">
-<input type="submit">Search:</button>
+<html>
+
+
+
+<form action="action_to_perform_after_submission" method = "POST">
+
+
+<p>Search: <input type = "text" name = "name" /> <input type = "submit" value = "submit" /></p>
+
+
+</form>
+
+
+</html>
+
--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,11 +23,9 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://172.18.0.1";
-	String validEmail = "user@example.com";
-	String validPassword = "password1234";
-	String invalidEmail = "none@example.com";
-	String invalidPassword = "password";
+	String url = "http://3.145.13.2";
+	String input = "string";
+	
 
     @Before
     public void setUp() { 
@@ -40,47 +38,5 @@ public class AppTest
 		driver.quit(); 
 	}	 
 	
-    @Test
-    public void testLoginWithValidEmailValidPassword() 
-		throws InterruptedException { 
-
-		//get web page
-		driver.get(url);
-		//wait until page is loaded or timeout error
-		wait.until(ExpectedConditions.titleContains("Login Page |")); 
-
-		//enter input
-		driver.findElement(By.name("email")).sendKeys(validEmail);
-		driver.findElement(By.name("password")).sendKeys(validPassword);
-		//click submit
-		driver.findElement(By.name("submit")).submit();
-	
-		//check result 
-		String expectedResult = "Dashboard |"; 
-		boolean isResultCorrect = wait.until(ExpectedConditions.titleContains(expectedResult)); 
-		assertTrue(isResultCorrect == true); 
-	}
-		
-	@Test
-    public void testLoginWithValidEmailInvalidPassword() 
-		throws InterruptedException { 
-
-		//get web page
-		driver.get(url);
-		//wait until page is loaded or timeout error
-		wait.until(ExpectedConditions.titleContains("Login Page |")); 
-
-		//enter input
-		driver.findElement(By.name("email")).sendKeys(validEmail);
-		driver.findElement(By.name("password")).sendKeys(invalidPassword);
-		//click submit
-		driver.findElement(By.name("submit")).submit();
-	
-		//check result
-		By errorMsgId = By.className("error-msg");
-		String expectedResult = "Login failed"; 
-		boolean isResultCorrect = wait.until(ExpectedConditions.textToBe(errorMsgId, expectedResult)); 
-		assertTrue(isResultCorrect == true); 
-	}
 
 }
